934432824b805b06a5ff025990593f5caa739b41
joshfermin <joshfermin@gmail.com>
2015-01-26 00:21:02 -0700

    got the put working

--- server/routes.js
+++ server/routes.js
@@ -44,46 +44,14 @@ module.exports = function(app){
   });
 
   app.put('/person/:id', function(req, res){
-    // Person.findById(req.params.id, function(err, doc){
-    // Person.find({id:req.params.id}).find(function(err,doc){
-    //   doc.first_name = req.body.first_name;
-    //   doc.last_name = req.body.last_name;
-    //   doc.email = req.body.email;
-    //   doc.country  = req.body.country;
-    //   doc.save(function(err){
-    //     if(!err){
-    //       res.send("Updated " + req.params.id);
-    //     } else {
-    //       res.send(err);
-    //     }
-    //   });
-    // });
-
-    Person.findOneAndUpdate(
-      {id:req.params.id}, 
-      {first_name:req.body.first_name, last_name:req.body.last_name, email:req.body.email, country:req.body.country},
-      function(err, doc){
-        if(err) res.send(err);
-        res.send("updated"+req.params.id)
-      })
-  });
-//   app.put('/api/products/:id', function (req, res){
-//   return ProductModel.findById(req.params.id, function (err, product) {
-//     product.title = req.body.title;
-//     product.description = req.body.description;
-//     product.style = req.body.style;
-//     return product.save(function (err) {
-//       if (!err) {
-//         console.log("updated");
-//       } else {
-//         console.log(err);
-//       }
-//       return res.send(product);
-//     });
-//   });
-// });
-    
-
+    var update = req.body;
+    console.log(update);
+    Person.findOneAndUpdate({id:req.params.id}, { $set: update }, function(err,person){
+      if(err) res.send(err);
+      res.send(person);
+    });
+  }); 
+  // curl -X PUT -d last_name=fermin http://localhost:3000/person/1005
 };
 // http://localhost:3000/person?id=1001&first_name=edward&last_name=zhu&email=zhued@zhued.com&country=china
 
