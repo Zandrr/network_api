2d987327614193bda3864870fbd34a83f17b67a2
joshfermin <joshfermin@gmail.com>
2015-01-29 18:29:20 -0700

    uncommenting test.js

--- test/test.js
+++ test/test.js
@@ -16,44 +16,44 @@ var request = require('supertest'),
         mockgoose(mongoose);
     }
 
-    // var Person = mongoose.model('Person');
+    var Person = mongoose.model('Person');
 
-    // beforeEach(function(done){
-    //   mockgoose.reset();
-    //   for(var i = 0; i < data.length; i++){
-    //     new Person(data[i]).save();
-    //   }
-    // });
+    beforeEach(function(done){
+      mockgoose.reset();
+      for(var i = 0; i < data.length; i++){
+        new Person(data[i]).save();
+      }
+    });
 
     
 
 
 
-    // describe('GET single person', function(){
-    //   it('responds with a single person object in JSON', function(done){
-    //     request(app)
-    //     .get('/person/1')
-    //     .set('Accept', 'application/json')
-    //     .expect('Content-Type', /json/)
-    //     .expect([{
-    //     _id: "54c43aaf25aa864371193fa9",
-    //     id: 1,
-    //     first_name: "Melissa",
-    //     last_name: "Butler",
-    //     email: "mbutler0@opensource.org",
-    //     country: "China"}])
-    //     .expect(200,done);
-    //   });
-    // });
+    describe('GET single person', function(){
+      it('responds with a single person object in JSON', function(done){
+        request(app)
+        .get('/person/1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect([{
+        _id: "54c43aaf25aa864371193fa9",
+        id: 1,
+        first_name: "Melissa",
+        last_name: "Butler",
+        email: "mbutler0@opensource.org",
+        country: "China"}])
+        .expect(200,done);
+      });
+    });
 
-    // describe('DELETE single person' , function(){
-    //   it('deletes a single person', function(done){
-    //     request(app)
-    //     .del('/person/57')
-    //     .expect(mockgoose.count().should.equal(999))
-    //     .expect(200);
-    //   });
-    // });
+    describe('DELETE single person' , function(){
+      it('deletes a single person', function(done){
+        request(app)
+        .del('/person/57')
+        .expect(mockgoose.count().should.equal(999))
+        .expect(200, done);
+      });
+    });
 
     describe('GET all people', function(){
       it('responds with a list of people objects in JSON', function(done){
